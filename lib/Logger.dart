fd6a208cb443b8c80204a8fa89ef4b37104ad7c8
max <trickster77777@gmail.com>
2026-06-18 05:12:49 +0600

    Route `logPrint` through the observe sink
    Co-Authored-By: AI Orchestrator <noreply@orchestrator>

--- lib/Logger.dart
+++ lib/Logger.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:observe/observe.dart';
 
 const _logDestination = String.fromEnvironment('LOG_DESTINATION', defaultValue: kDebugMode ? 'both' : 'file');
 
@@ -6,10 +7,13 @@ bool get logToConsole => _logDestination != 'grafana';
 bool get logToObserver => _logDestination != 'file';
 
 void logPrint(Object? object) {
-  final now = DateTime.now();
-  final time = '${now.hour.toString().padLeft(2, '0')}:'
-      '${now.minute.toString().padLeft(2, '0')}:'
-      '${now.second.toString().padLeft(2, '0')}.'
-      '${(now.millisecond ~/ 10).toString().padLeft(2, '0')}';
-  debugPrint('[$time] $object');
+  if (logToConsole) {
+    final now = DateTime.now();
+    final time = '${now.hour.toString().padLeft(2, '0')}:'
+        '${now.minute.toString().padLeft(2, '0')}:'
+        '${now.second.toString().padLeft(2, '0')}.'
+        '${(now.millisecond ~/ 10).toString().padLeft(2, '0')}';
+    debugPrint('[$time] $object');
+  }
+  if (logToObserver) observeSink(object?.toString() ?? '');
 }
